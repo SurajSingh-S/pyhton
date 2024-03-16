0dfebd004d25b1cbcaf1d10980256cd2b7d064e9
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2024-03-16 16:17:40 +0530

    Update lab1-ques1.py

--- lab1-ques1.py
+++ lab1-ques1.py
@@ -4,3 +4,11 @@ r=float(input("r: "))
 t=float(input("t: "))
 si=(p*r*t)/100
 print(si)
+
+
+#compound interest
+p=float(input("p: "))
+r=float(input("r: "))
+n=float(input("n: "))
+ci=p*(1+r/100)**n-p
+print(ci)
