9f61e49d935317a0d300e6880f93bfb0ee83bc1b
SurajSingh-S <146805274+SurajSingh-S@users.noreply.github.com>
2024-03-16 16:20:19 +0530

    Update lab1-ques2.py

--- lab2-ques2.py
+++ lab2-ques2.py
@@ -1,1 +1,19 @@
+#Emi 
+p = float(input("Enter principal amount: "))
+R = float(input("Enter annual interest rate: "))
+n = int(input("Enter number of months: " ))
 
+# Calculating interest rate per month
+r = R/(12*100)
+
+# Calculating Equated Monthly Installment (EMI)
+emi = p * r * ((1+r)**n)/((1+r)**n - 1)
+print("Monthly EMI = ", emi)
+
+#gst
+cost=float(input("enter cost of the products/services: "))
+gstPer=float(input("enter the % charged for gst: "))
+gst=cost*gstPer/100
+amount=cost+gst
+print("gst is",gst)
+print("amount to pay is ",amount)
